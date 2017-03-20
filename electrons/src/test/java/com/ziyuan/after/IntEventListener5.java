dbbc734a125337ddaf51d792ec67c6049f13d2ec
ziyuan <ziyuan@2dfire.com>
2017-03-20 20:49:59 +0800

    最新的场景：A之后执行B、C，B之后执行D，D、C之后执行E

---
+++ electrons/src/test/java/com/ziyuan/after/IntEventListener5.java
@@ -0,0 +1,19 @@
+package com.ziyuan.after;
+
+import com.ziyuan.ElectronsListener;
+import com.ziyuan.Listener;
+
+/**
+ * IntEventListener5
+ *
+ * @author ziyuan
+ * @since 2017-03-20
+ */
+@Listener(id = "4", subscribe = "after", after = "2")
+public class IntEventListener5 implements ElectronsListener<IntEvent> {
+
+    @Override
+    public void onEvent(IntEvent electrons) throws Exception {
+        System.out.println("last last is doing...");
+    }
+}
